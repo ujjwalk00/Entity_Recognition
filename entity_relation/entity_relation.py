436293c87abc1f05c6c088146573f8f360857f65
Reena Koshta <reenakoshta@yahoo.com>
2022-01-19 17:12:38 +0100

    integrated code

--- entity_relation/entity_relation.py
+++ entity_relation/entity_relation.py
@@ -4,114 +4,137 @@ from spacy.matcher import Matcher
 
 
 def find_rel(doc, nlp):
-    subject =[]
+    subject = []
     target = []
     relation = []
     entities = set()
     for token in doc:
-        if token.dep_== 'ccomp' or token.dep_=='ROOT' or token.pos_ =='VERB':
+        if token.dep_ == "ccomp" or token.dep_ == "ROOT" or token.pos_ == "VERB":
             sub = find_sub(token)
-            if token.nbor().dep_ in ['agent','prep']:
-                rel = " ".join([token.text,token.nbor().text])
+            if token.nbor().dep_ in ["agent", "prep"]:
+                rel = " ".join([token.text, token.nbor().text])
                 obj = find_obj(token.nbor(), doc)
             else:
                 rel = token.text
                 obj = find_obj(token, doc)
-            
+
             if sub is None:
                 continue
-            sub= get_full_word(sub, doc)
+            sub = get_full_word(sub, doc)
             if obj is None:
                 continue
             subject.append(sub)
-            target.append(obj)
+            target.append(str(obj))
             relation.append(rel)
             entities.add(sub)
             entities.add(obj)
 
     location_rel = find_rel_location(doc, nlp)
     for rel in location_rel:
-      subject.append(rel[0])
-      relation.append(rel[1])
-      target.append(rel[2])
-      entities.add(rel[0])
-      entities.add(rel[2])
-    input_df = pd.DataFrame({'source':subject, 'relation':relation, 'target': target})
+        subject.append(rel[0])
+        relation.append(rel[1])
+        target.append(rel[2])
+        entities.add(rel[0])
+        entities.add(rel[2])
+    input_df = pd.DataFrame({"source": subject, "relation": relation, "target": target})
     print(input_df)
-    input_df.to_csv("Assets/input-data-for-graph_1.csv", index= False)
-    entity_df = pd.DataFrame({'id':range(1,len(entities)+1),'entity':[*entities, ]})
-    entity_df.to_csv("Assets/entities-for_graph.csv", index = False)
+    input_df.to_csv("assets/input-data-for-graph_1.csv", index=False)
+    entity_df = pd.DataFrame(
+        {
+            "id": range(1, len(entities) + 1),
+            "entity": [
+                *entities,
+            ],
+        }
+    )
+    entity_df.to_csv("assets/entities-for_graph.csv", index=False)
     return input_df
-            
+
+
 def find_sub(pred):
-    if len(list(pred.lefts))>0:
+    if len(list(pred.lefts)) > 0:
         for token in pred.lefts:
-            if token.dep_ in ['nsubj', 'nsubjpass']:
+            if token.dep_ in ["nsubj", "nsubjpass"]:
                 return token
     else:
-        if pred.i >1:
-          if pred.nbor(-1).dep_ != 'punct':
-              return pred.nbor(-1)
-          else:
-              return pred.nbor(-2)
-        
+        if pred.i > 1:
+            if pred.nbor(-1).dep_ != "punct":
+                return pred.nbor(-1)
+            else:
+                return pred.nbor(-2)
+
+
 def find_obj(pred, article):
-    obj_dep= ['dobj', 'pobj', 'iobj', 'obj', 'obl']
+    obj_dep = ["dobj", "pobj", "iobj", "obj", "obl"]
     obj = None
     for token in pred.rights:
-        if token.dep_ != 'punct':  
+        if token.dep_ != "punct":
             if token.dep_ in obj_dep:
                 obj = token
-            elif token.dep_ == 'ccomp':
-                    return find_sub(token)
+            elif token.dep_ == "ccomp":
+                return find_sub(token)
             else:
-              for right in token.rights:
-                if right.dep_ == 'xcomp':
-                  return find_obj(right, article)
-                else:
-                  if right.pos_ != 'ADP':
-                    obj = right
-                  else:
-                    return find_obj(right, article)
+                for right in token.rights:
+                    if right.dep_ == "xcomp":
+                        return find_obj(right, article)
+                    else:
+                        if right.pos_ != "ADP":
+                            obj = right
+                        else:
+                            return find_obj(right, article)
         else:
             break
     for token in pred.lefts:
-        if token.dep_ == 'ccomp':
+        if token.dep_ == "ccomp":
             return find_sub(token)
     if obj != None:
         obj = get_full_word(obj, article)
-        
+
     return obj
 
+
 def is_passive(token):
-    if token.dep_.endswith('pass'): # noun
+    if token.dep_.endswith("pass"):  # noun
         return True
-    for left in token.lefts: # verb
-        if left.dep_ == 'auxpass':
+    for left in token.lefts:  # verb
+        if left.dep_ == "auxpass":
             return True
     return False
+
 
 def get_full_word(token, article):
     entities = [ent for ent in article.ents]
     for words in entities:
-        if token.i in range(words.start,words.end):
+        if token.i in range(words.start, words.end):
             return words.text
         elif token.text == article[words.start].text:
             return words.text
     nouns = [ent for ent in article.noun_chunks]
     for words in nouns:
         if token in words:
-            return words.text    
+            return words.text
     return token.text
+
 
 def find_rel_location(article, nlp):
     matcher = Matcher(nlp.vocab)
-    rel_list =[]
-    pattern = [{'ENT_TYPE':'ORG'}, {'POS': {'IN':['ADP','PUNCT']}},{'POS': 'DET', 'OP': '?'},{'ENT_TYPE':'GPE'}]
-    matcher.add("located",[pattern])
+    rel_list = []
+    pattern = [
+        {"ENT_TYPE": "ORG"},
+        {"POS": {"IN": ["ADP", "PUNCT"]}},
+        {"POS": "DET", "OP": "?"},
+        {"ENT_TYPE": "GPE"},
+    ]
+    matcher.add("located", [pattern])
     matches = matcher(article)
     for match_id, start, end in matches:
         span = article[start:end]
-        
-        rel_list.append([get_full_word(span[0], article),'located in',get_full_word(span[-1], article)])
+
+        rel_list.append(
+            [
+                get_full_word(span[0], article),
+                "located in",
+                get_full_word(span[-1], article),
+            ]
+        )
     return rel_list
--- main.py
+++ main.py
@@ -13,11 +13,10 @@ from streamlit_agraph import agraph, Node, Edge, Config
 from entity_relation import *
 
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load("en_core_web_sm")
 
 
-
-#article = nlp('Borden Inc said it is acquiring Prince Co Inc and three companies producing grocery products for 180 mln dlrs. Borden said the four companies are expected to have 1987 sales totaling 230 mln dlrs. It said Prince, a Lowell, Mass., producer of pasta and Italian food sauces, is expected to account for 210 mln dlrs of this total. This years sales of Borden pasta -- by the 13 regional brands and the premium Creamette brand distributed on a nearly national basis -- are expected to toal 285 mln dlrs, it said. Borden said the other three companies being acquired are Steero Bouillon of Jersey City, N.J., Blue Channel Inc, a Beaufort, S.C., producer of canned crabmeat, and the canned shrimp products line of DeJean Packing Inc of Biloxi, Miss. Borden also said the divestment of three operations with about 50 mln dlrs a year in sales is expected to produce nearly 45 mln dlrs in cash for use toward the purchase of new businesses. It said the sale of Polyco of Cincinnati, Ohio, which makes polyvinyl acetate emulsions, to Rohm and Haas Co ROH was announced by the buyer last month.  Borden said the divestment of two producers of toy models and hobby items -- Heller in France and Humbrol in England -- is in process.')
+# article = nlp('Borden Inc said it is acquiring Prince Co Inc and three companies producing grocery products for 180 mln dlrs. Borden said the four companies are expected to have 1987 sales totaling 230 mln dlrs. It said Prince, a Lowell, Mass., producer of pasta and Italian food sauces, is expected to account for 210 mln dlrs of this total. This years sales of Borden pasta -- by the 13 regional brands and the premium Creamette brand distributed on a nearly national basis -- are expected to toal 285 mln dlrs, it said. Borden said the other three companies being acquired are Steero Bouillon of Jersey City, N.J., Blue Channel Inc, a Beaufort, S.C., producer of canned crabmeat, and the canned shrimp products line of DeJean Packing Inc of Biloxi, Miss. Borden also said the divestment of three operations with about 50 mln dlrs a year in sales is expected to produce nearly 45 mln dlrs in cash for use toward the purchase of new businesses. It said the sale of Polyco of Cincinnati, Ohio, which makes polyvinyl acetate emulsions, to Rohm and Haas Co ROH was announced by the buyer last month.  Borden said the divestment of two producers of toy models and hobby items -- Heller in France and Humbrol in England -- is in process.')
 
 
 # Create a page dropdown
@@ -29,20 +28,20 @@ page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 
 if page == "Page 1":
 
-    sentence = st.text_input('Input your sentence here:')
+    sentence = st.text_input("Input your sentence here:")
     if sentence:
-        st.write(sentence)
+        # st.write(sentence)
         sentence = normalize_docs_text(sentence)
-        st.write(sentence)
+        # st.write(sentence)
 
-        st.write(type(sentence))
+        # st.write(type(sentence))
         article = nlp(sentence)
         # html_text = displacy.render(article, style='ent')
         # components.html(html_text,
         # height=600
         # )
-        
-        input_df = find_rel(article)
+
+        input_df = find_rel(article, nlp)
         st.dataframe(input_df)
 
         nodes = []
@@ -52,43 +51,34 @@ if page == "Page 1":
 
         for i in range(len_df):
 
-
-
             nodes.append(
-                Node(id=input_df.iloc[i, 0],
-                     label=input_df.iloc[i, 0],
-                     size=800
-                     )
+                Node(id=input_df.iloc[i, 0], label=input_df.iloc[i, 0], size=800)
             )
 
             nodes.append(
-                Node(id=input_df.iloc[i, 2],
-                    label=input_df.iloc[i, 2],
-                    size=800
-                    )
-
+                Node(id=input_df.iloc[i, 2], label=input_df.iloc[i, 2], size=800)
             )
 
-            edges.append(Edge(source=input_df.iloc[i, 0],
-                            label=input_df.iloc[i, 1],
-                            target=input_df.iloc[i, 2],
-                            type="CURVE_SMOOTH",
-                            strokeWidth=1.5
+            edges.append(
+                Edge(
+                    source=input_df.iloc[i, 0],
+                    label=input_df.iloc[i, 1],
+                    target=input_df.iloc[i, 2],
+                    type="CURVE_SMOOTH",
+                    strokeWidth=1.5,
+                )
+            )
 
-                            )
-                        )
+        config = Config(
+            width=800,
+            height=800,
+            directed=True,
+            node={"labelProperty": "label"},
+            link={"labelProperty": "label", "renderLabel": True}
+            # **kwargs e.g. node_size=1000 or node_color="blue"
+        )
 
-        config = Config(width=800,
-                        height=800,
-                        directed=True,
-                        node={'labelProperty': 'label'},
-                        link={'labelProperty': 'label', 'renderLabel': True}
-                        # **kwargs e.g. node_size=1000 or node_color="blue"
-                        )
-
-        return_value = agraph(nodes=nodes,
-                            edges=edges,
-                            config=config)
+        return_value = agraph(nodes=nodes, edges=edges, config=config)
 
         # nodes_list = []
         # edges_list = []
@@ -213,42 +203,70 @@ if page == "Page 1":
 
 if page == "Page 2":
 
-    df = pd.read_excel("Assets//"+"preprocessed_text.xlsx")
+    df = pd.read_excel("assets//" + "preprocessed_text.xlsx")
 
-    option = st.selectbox(
-        'What category do you want to test?',
-        df.categories.unique())
+    option = st.selectbox("What category do you want to test?", df.categories.unique())
     option_clean = option.replace("['", "").replace("']", "")
 
-    st.write('You selected:', option_clean)
+    st.write("You selected:", option_clean)
 
-    data = df[df.categories == (option)]['text']
+    data = df[df.categories == (option)]["text"]
     entity_set = []
     for entity in data:
         article = nlp(entity)
         for ne in article.ents:
-            if ne.label_ == 'ORG':
+            if ne.label_ == "ORG":
                 entity_set.append(ne.text)
-    input_df = pd.DataFrame({'source': entity_set, 'relation': [
-                            'has article related to'] * len(entity_set), 'target': option_clean}).drop_duplicates()
+    input_df = pd.DataFrame(
+        {
+            "source": entity_set,
+            "relation": ["has article related to"] * len(entity_set),
+            "target": option_clean,
+        }
+    ).drop_duplicates()
 
     nodes_list = []
     edges_list = []
     nodes_list.append(
-        {"id": option_clean, "label": option_clean, "shape": "dot", "size": 10})
+        {"id": option_clean, "label": option_clean, "shape": "dot", "size": 10}
+    )
     len_df = len(input_df)
     if len_df >= 21:
         for i in range(21):
             nodes_list.append(
-                {"id": input_df.iloc[i, 0], "label": input_df.iloc[i, 0], "shape": "dot", "size": 10})
+                {
+                    "id": input_df.iloc[i, 0],
+                    "label": input_df.iloc[i, 0],
+                    "shape": "dot",
+                    "size": 10,
+                }
+            )
             edges_list.append(
-                {"from": input_df.iloc[i, 0], "relation": input_df.iloc[i, 1], "to": option_clean, "weight": 1})
+                {
+                    "from": input_df.iloc[i, 0],
+                    "relation": input_df.iloc[i, 1],
+                    "to": option_clean,
+                    "weight": 1,
+                }
+            )
     else:
         for i in range(len_df):
             nodes_list.append(
-                {"id": input_df.iloc[i, 0], "label": input_df.iloc[i, 0], "shape": "dot", "size": 10})
+                {
+                    "id": input_df.iloc[i, 0],
+                    "label": input_df.iloc[i, 0],
+                    "shape": "dot",
+                    "size": 10,
+                }
+            )
             edges_list.append(
-                {"from": input_df.iloc[i, 0], "relation": input_df.iloc[i, 1], "to": option_clean, "weight": 1})
+                {
+                    "from": input_df.iloc[i, 0],
+                    "relation": input_df.iloc[i, 1],
+                    "to": option_clean,
+                    "weight": 1,
+                }
+            )
 
     # html_str = """
     # <html>
@@ -277,14 +295,13 @@ if page == "Page 2":
     #             width: 1000px;
     #             height: 600px;
     #         }
-            
+
     # </style>
 
     # </head>
 
     # <body>
     # <div id = "mynetwork"></div>
-
 
     # <div id = "config"></div>
 
@@ -293,16 +310,13 @@ if page == "Page 2":
     #     // initialize global variables.
     #     var edges;
     #     var nodes;
-    #     var network; 
+    #     var network;
     #     var container;
     #     var options, data;
 
-        
     #     // This method is responsible for drawing the graph, returns the drawn network
     #     function drawGraph() {
     #         var container = document.getElementById('mynetwork');
-            
-            
 
     #         // parsing and collecting nodes and edges from the python
     #         nodes = new vis.DataSet(""" + str(nodes_list)+""");
@@ -343,14 +357,13 @@ if page == "Page 2":
     #         }
     #     }
     # };
-            
+
     #         // if this network requires displaying the configure window,
     #         // put it in its div
     #         options.configure["container"] = document.getElementById("config");
-            
 
     #         network = new vis.Network(container, data, options);
-        
+
     #         return network;
 
     #     }
